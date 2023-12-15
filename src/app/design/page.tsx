d16182bcd1d9a70973ae0e7d0c6827c398f4b4c5
Filipe Salles <filipesalles69@gmail.com>
2023-12-15 19:13:38 -0300

    adicionando novos shortcuts

--- src/app/design/page.tsx
+++ src/app/design/page.tsx
@@ -46,6 +46,30 @@ function index() {
     text:'Uma I.A que gera voz baseado em prompts.',
     img:'voiceia'
   },
+  {
+    title:'Capcut',
+    href:'https://www.capcut.com/',
+    text:'Uma ferramenta para criar e editar vídeos.',
+    img:'capcut'
+  },
+  {
+    title:'Runwayml',
+    href:'https://app.runwayml.com/',
+    text:'Uma I.A que anima imagens ou gera imagens animadas.',
+    img:'runwayml'
+  },
+  {
+    title:'Crayon',
+    href:'https://www.craiyon.com/',
+    text:'Uma I.A que gera imagens atraves de prompts 100% gratuida',
+    img:'crayon'
+  },
+  {
+    title:'Leonardo.AI',
+    href:'https://leonardo.ai/',
+    text:'Uma I.A que gera imagens atraves de prompts complexas, porém, limitado.',
+    img:'leonardoai'
+  },
 ];
   return (
     <main>
